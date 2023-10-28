1b17f5f80cc287982c0c3ded2dc8ab1ed881b71c
Robin Bisht <130886823+robinbisht20@users.noreply.github.com>
2023-10-28 06:21:03 +0530

    Create HacktoberfestStarter.py

---
+++ HacktoberfestStarter.py
@@ -0,0 +1,10 @@
+# HacktoberfestStarter - A Python Code Example
+# Welcome to Hacktoberfest 2023!
+
+def greet_contributors():
+    print("Welcome to HacktoberfestStarter!")
+    print("This is a simple Python code example.")
+    print("Let's collaborate and make this project awesome!")
+
+if __name__ == "__main__":
+    greet_contributors()
